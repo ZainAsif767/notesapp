44aaf8ef79be49a63e5dcc3679ccee4f8fcae10d
zainasif767 <zainclasher2002@gmail.com>
2023-01-09 23:49:57 +0500

    commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Sidebar(props) {
+    const noteElements = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div
+
+                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
+                    }`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className="text-snippet">Note {index + 1}</h4>
+            </div>
+        </div>
+    ))
+
+    return (
+        <section className="pane sidebar">
+            <div className="sidebar--header">
+                <h3>Notes</h3>
+                <button className="new-note" onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        id: 1,
+        body: `### This just in !
+        
+        Markdown is an *awesome* way to write **very basic HTML** by typing manually
+        
+        It can be used for:
+        
+        1. README files
+        2. Slack and Discord messages
+        3. **So Much more !**
+
+        ---
+        
+        Here's is division 🤞`,
+    },
+    { id: 2, body: `a` },
+    { id: 3, body: `b` },
+    { id: 4, body: `c` },
+    { id: 5, body: `d` },
+    { id: 6, body: `e` },
+]
